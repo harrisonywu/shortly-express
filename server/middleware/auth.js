b4a8851b5227ddfb4d0f33c952815453f9cb80fa
harrisonywu <harrisonywu@gmail.com>
2019-03-16 15:08:55 -0700

    Implement working cookieParser and begin pseudocode of auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,12 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+    var cookie = req.cookies
+        models.Sessions.create()
+        .then(function(newCookie) {
+            req.cookie = newCookie
+        })
+    next();
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,5 +1,19 @@
 const parseCookies = (req, res, next) => {
-    console.log('cookies', req.headers.cookie)
+    var cookieString = req.headers.cookie;
+    console.log('res.cookies :', res.cookies);
+    
+    if (cookieString) {
+        var cookieArray = cookieString.split('; ');
+        cookieArray.forEach((string) => {
+            var keyValueArray = string.split('=');
+            var keyName = keyValueArray[0];
+            var valueName = keyValueArray[1];
+            req.cookies[keyName] = valueName;
+        })
+    }
+    // console.log(res.cookies);
+    next();
+    
 };
 
 module.exports = parseCookies;
