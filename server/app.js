e0289185e63cfb660baa74749fecf129a48a5eb0
harrisonywu <harrisonywu@gmail.com>
2019-03-15 19:33:10 -0700

    Implement working signup and begin login post request

--- server/app.js
+++ server/app.js
@@ -19,102 +19,98 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.get('/', 
-(req, res) => {
-  console.log('hello');
-  res.render('index');  //hw: fred said these render functions will basically create your html
-});
+  (req, res) => {
+    console.log('hello');
+    res.render('index'); //hw: fred said these render functions will basically create your html
+  });
 
 app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+  (req, res) => {
+    res.render('index');
+  });
 
 app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
+      });
+  });
 
 app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
     // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
+      return res.sendStatus(404);
+    }
 
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
         throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
-});
+  });
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+app.post('/login', (req, res) => {
+  var inputUsername = req.body.username;
+  models.Users.get({username: inputUsername})
+  .then(function (result) {
+    if (result) {
+      res.redirect('/');
+      //log them in
+      // what does logging in mean?
+      // render a page
+      // or redirect them to their homepage
+    } else {
+      res.redirect('/login');
+    }
+  })  
+});
+
+
+
+
+////////////////////////////
 app.post('/signup', (req, res, next) => {
   var inputUsername = req.body.username;
-  db.query('SELECT username from users', (err, data) => {
-    if (err) { throw err };
-    console.log('username: ', data[0].username) 
-    console.log('data :', data)
-    // what does data look like?
-    //  side question: why is buddy in the table right now
-    //  and why do we skipping 2?????????
-
-    // if no error, we want to compare the table usernames to inputUsername
-      // do this for each username in the table
-      // if one of them matched
-        // then console.log (this username already taken);
-        // and we want to redirect to signup
-      // if none match, we create.
-    
-/* AFTER DINNER
-    we know what our data looks like and how to access the username
-    so need to implement username checking (possibly with for loop)
-
-*/
-// 
-
-    
-  });
-
-
-  models.Users.create(req.body);
-  done();
-  // .then(function(result) {
-  // })
-  console.log('hey buddy', username, password);
-  console.log('')
-})
-
+  models.Users.get({username: inputUsername})
+  .then(function (result) {
+    if (result) {
+      res.redirect('/signup');
+    } else {
+      models.Users.create(req.body);
+      res.redirect('/');
+    }
+  })
+});
 
 /************************************************************/
 // Handle the code parameter route last - if all other routes fail
